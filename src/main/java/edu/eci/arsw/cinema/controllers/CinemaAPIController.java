80598ed5a9ee7e0952c6e33e05a199a353a03abb
cesar eduardo <cesar.lanos@mail.escuelaing.edu.co>
2019-02-28 18:40:10 -0500

    Se verifico que SE OBTIENE UN objeto JSON con una lista que contenga el detalle de los cines suminstrados por defecto

--- src/main/java/edu/eci/arsw/cinema/controllers/CinemaAPIController.java
+++ src/main/java/edu/eci/arsw/cinema/controllers/CinemaAPIController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping(value = "/cinema")
+@RequestMapping(value = "/cinemas")
 public class CinemaAPIController {
 
     @Autowired
